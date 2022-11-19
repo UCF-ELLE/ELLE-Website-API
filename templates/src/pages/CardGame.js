06033dd02cd96f951cd034b1db2c24b0c33916fd
Skylar Marosi <33764428+smarosi1@users.noreply.github.com>
2022-11-19 02:00:32 -0500

    Added CardGame page

---
+++ templates/src/pages/CardGame.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+
+import { Button } from 'reactstrap';
+import MainTemplate from '../pages/MainTemplate'; 
+import Template from '../pages/Template';
+
+import '../stylesheets/style.css';
+import '../lib/bootstrap/css/bootstrap.min.css';
+import '../lib/font-awesome/css/font-awesome.min.css';
+import '../lib/owlcarousel/assets/owl.carousel.min.css';
+import '../lib/ionicons/css/ionicons.min.css';
+
+// import banner from '../Images/ELLEDownloadsBanner.mp4';
+
+import Unity, { UnityContent } from "react-unity-webgl";
+
+const unityContent = new UnityContent(
+	"Build/Builds.json",
+	"Build/UnityLoader.js"
+);
+
+// untested code from previous group for MazeGame
+/*
+unityContent.on("GameLoaded", () => {
+	sendLogin();
+});
+function sendLogin() {
+	const jwt = localStorage.getItem('jwt');
+	unityContent.send("ContinueButton", "loginAttempt", jwt);
+  }*/
+
+export default class CardGame extends Component {
+	constructor(props) {
+		super(props);
+
+		// more old code
+		/*this.state = {
+			permission: this.props.user.permission,
+		}*/
+        
+	}  
+	
+	// more old code
+	/*
+	componentDidMount() {
+		this.verifyPermission(); 
+	}
+
+	verifyPermission = () => {
+		const jwt = localStorage.getItem('jwt');
+		if (!jwt) {
+		  this.props.history.push(this.props.location.pathname);
+		}
+		else {
+			var jwtDecode = require('jwt-decode');	
+			var decoded = jwtDecode(jwt);
+			this.setState({ permission: decoded.user_claims.permission }); 
+		}
+	}*/   
+	  
+	handleOnClickFullscreen() {
+		  unityContent.setFullscreen(true);
+	}
+
+	render() {
+	return (  
+	<div className="downloadsBg">
+		
+		{localStorage.getItem('jwt') === null ? <MainTemplate /> : <Template permission={this.state.permission}/>}
+						<br />
+						<Unity unityContent={unityContent} width="75%" height="75%"/>
+						<center>
+						
+						<br />
+						<br />
+						<Button onClick={this.handleOnClickFullscreen}>Fullscreen</Button>
+						<p></p>
+						<br />
+						</center>
+						<h3 className="cardGameText" >ELLE Card Game</h3>
+						<p className="cardGameText">Senior Design Team:</p>
+						<ul className="cardGameText">
+								<li>Justin Moy</li>
+								<li>Connor Lysek</li>
+								<li>Keilvin Tran</li>
+                                <li>Natham Lim</li>
+                                <li>Skylar Marosi</li>
+						</ul>
+						<p className="cardGameText">If there are no available modules for you to select, try logging out and logging back in. Also, make sure you are on the secure version of the site - if you look at the URL bar, to the left you should see the word "Secure" or a closed lock. If you do not see that, click <a href="https://endlesslearner.com/mazegame">here</a> to be redirected to the secure version of this page.</p>
+						<br />
+						<p></p>
+		<footer id="footer">
+			<div className="container">
+				<div className="copyright">&copy; Copyright 2022 <strong>Reveal</strong>. All Rights Reserved</div>
+				<div className="credits">
+				{/*
+				All the links in the footer should remain intact.
+				You can delete the links only if you purchased the pro version.
+				Licensing information: https://bootstrapmade.com/license/
+				Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Reveal
+				*/}
+				Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+				</div>
+			</div>
+		</footer>
+	</div>
+  );
+	}
+}
