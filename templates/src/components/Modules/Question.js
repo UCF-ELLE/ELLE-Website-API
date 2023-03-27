edb0f0093acddd27e3b292d786351342352acd66
Naton-1 <38639895+Naton-1@users.noreply.github.com>
2023-03-27 16:34:06 -0400

    Update Question.js
    Remove an extra <td></td> that was messing with the spacing of the buttons

--- templates/src/components/Modules/Question.js
+++ templates/src/components/Modules/Question.js
@@ -286,20 +286,18 @@ class Question extends React.Component {
         <Fragment>
         <tr onClick={this.toggleCollapsedAnswers}>
           <td>{question.questionText}</td>
-          <td>
             <td>
               {/* Add disabled attribute to disable button if no image file found */}
-               <Button className={`image-btn ${imgButtonClass}`} href={imgLink} download disabled={disableImgButton}>
-                  <img src={require('../../Images/image.png')} alt="frame icon" style={{ width: '25px', height: '25px' }} />
-                </Button>
+              <Button className={`image-btn ${imgButtonClass}`} href={imgLink} download disabled={disableImgButton}>
+                <img src={require('../../Images/image.png')} alt="frame icon" style={{ width: '25px', height: '25px' }} />
+              </Button>
             </td>
             <td>
               {/* Add disabled attribute to disable button if no audio file found */}
               <Button className={`audio-btn ${audioButtonClass}`} href={audioLink} download disabled={disableAudioButton}>
                 <img src={require('../../Images/headphones.png')} alt="headphones icon" style={{ width: '25px', height: '25px' }} />
-               </Button>
+              </Button>
             </td>
-          </td>
 
           {this.props.permissionLevel !== "st"
           ?
