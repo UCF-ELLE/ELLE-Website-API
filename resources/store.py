79e16b534174ebbea89df26f79491dc77d46e7a3
Zander Preston <zanpres@gmail.com>
2024-04-12 20:37:03 -0400

    Define Response in GetUserItemCSV

--- resources/store.py
+++ resources/store.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 from flask_jwt_extended import (
     jwt_required,
 )
-from flask import request
+from flask import request, Response
 from db import mysql
 from db_utils import *
 from utils import *
