b8099702f62d574ef8252277be98bbe9107aa537
LoganWitte <loganswitte@gmail.com>
2025-02-19 13:58:38 -0500

    Update talkwithtito.tsx

--- templates/pages/games/talkwithtito.tsx
+++ templates/pages/games/talkwithtito.tsx
@@ -1,3 +1,16 @@
+import React, {
+    useEffect,
+    useState,
+    useCallback,
+    useContext,
+    useRef,
+} from "react";
+
+import "@/public/static/css/style.css";
+import "@/lib/font-awesome/css/font-awesome.min.css";
+import "@/lib/owlcarousel/assets/owl.carousel.min.css";
+import "@/lib/ionicons/css/ionicons.min.css";
+
 export default function talkwithtito() {
-    <div>Hello World</div>
+    <div className="w-full h-32 flex justify-center bg-blue-200">Hello World I`&apos;`m Tito</div>
 }
