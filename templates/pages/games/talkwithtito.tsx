0353f0cc64b20082cdea53c593ab41e45c6f852e
LoganWitte <loganswitte@gmail.com>
2025-02-19 14:13:53 -0500

    Update talkwithtito.tsx

--- templates/pages/games/talkwithtito.tsx
+++ templates/pages/games/talkwithtito.tsx
@@ -11,8 +11,8 @@ import "@/lib/font-awesome/css/font-awesome.min.css";
 import "@/lib/owlcarousel/assets/owl.carousel.min.css";
 import "@/lib/ionicons/css/ionicons.min.css";
 
-export default function talkwithtito() {
+export default function TalkWithTitoGame() {
     return(
-        <div className="w-full h-32 flex justify-center bg-blue-200">Hello World I`&apos;`m Tito</div>
-    )
+        <div className="w-full h-32 flex justify-center bg-blue-200">Hello World I am Tito</div>
+    );
 }
