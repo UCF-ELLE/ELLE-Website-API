f36836561e11c0bd2ccfaebee0d6caff732d7454
LoganWitte <loganswitte@gmail.com>
2025-02-21 15:57:43 -0500

    Update talkwithtito.tsx

--- templates/pages/games/talkwithtito.tsx
+++ templates/pages/games/talkwithtito.tsx
@@ -14,24 +14,22 @@ import tito_speak from "@/public/static/images/ConversAItionELLE/tito.png";
 import happyTito from "@/public/static/images/ConversAItionELLE/happyTito.png";
 
 // Component imports
-import {useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
-
-
-
-export default function TalkWithTito(){
-  const titoStatements:string[] = ['Tito is creating a new dish...', 'Tito is freshening up...', 'Tito is taking a nap...']
+export default function TalkWithTito() {
+  const titoStatements: string[] = ['Tito is creating a new dish...', 'Tito is freshening up...', 'Tito is taking a nap...']
   const titoStatementsRef = useRef<string[]>(titoStatements)
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [playClicked, setPlayClicked] = useState<boolean>(false);
 
   //Handles play button click
   const handlePlayClick = () => {
-    if(isLoading) {
+    if (isLoading) {
       return;
     }
     else {
-
+      setPlayClicked(true);
     }
   }
 
@@ -39,24 +37,26 @@ export default function TalkWithTito(){
   const [statement, setStatement] = useState<string>(titoStatementsRef.current[0]);
   const [index, setIndex] = useState<number>(1);
 
-  useEffect(()=>{
-    const interval = setInterval(()=>{
+  useEffect(() => {
+    const interval = setInterval(() => {
       setStatement(titoStatementsRef.current[index]);
-      setIndex((prev)=> (prev+1) % titoStatementsRef.current.length);
+      setIndex((prev) => (prev + 1) % titoStatementsRef.current.length);
     }, 4000)
 
     return () => clearInterval(interval);
   }, [index])
 
   return (
-      <div className="relative w-full mt-2 mb-2 flex justify-center">
+    <div className="relative w-full mt-2 mb-2 flex justify-center">
+      <button onClick={() => setIsLoading(!isLoading)} className="absolute top-1 right-1 z-1 text-red-800 font-bold border-red-800 border-2 bg-white">Toggle loading (temporary)</button>
+      {!playClicked ?
         <div className="relative w-[80%] h-fit border-2 border-black">
-          <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background"/>
+          <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background" />
           {
             isLoading ?
-            <Image src={tito_speak} alt="TalkWithTito placeholder" className="tito-overlay"/>
-            :
-            <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[45%] left-[50%] -translate-x-1/2 -translate-y-1/2"/>
+              <Image src={tito_speak} alt="TalkWithTito placeholder" className="tito-overlay" />
+              :
+              <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[45%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
           }
           <div className="absolute top-[11.5%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
                         text-white text-2xl md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">{isLoading ? statement : "Talk With Tito"}</div>
@@ -64,10 +64,16 @@ export default function TalkWithTito(){
                           absolute top-[80%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
                         text-white md:text-4xl font-bold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover
                           ${!isLoading ? "hover:bg-[#816031] hover:cursor-pointer" : ""}`}
-          onClick={handlePlayClick}>
+            onClick={handlePlayClick}>
             {isLoading ? "Loading..." : "Play!"}
           </div>
         </div>
-      </div>
-    );
+        :
+        <div className="bg-white relative w-[90vw] h-fit max-h-[80vh] border-2 border-black">
+          <button onClick={() => setPlayClicked(false)}>Click to return to main menu</button>
+        </div>
+      }
+
+    </div>
+  );
 }
