73e3dfa8c3c99568df9985dc5aacbe4191a49c03
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2025-02-21 12:05:00 -0500

    Talk With Tito Placeholder
    - added template site
    - need to fix css due to ELLE css

--- templates/pages/games/talkwithtito.tsx
+++ templates/pages/games/talkwithtito.tsx
@@ -4,10 +4,42 @@ import "@/public/static/css/style.css";
 import "@/lib/font-awesome/css/font-awesome.min.css";
 import "@/lib/owlcarousel/assets/owl.carousel.min.css";
 import "@/lib/ionicons/css/ionicons.min.css";
+import {useState, useEffect, useRef} from "react";
+import "@/public/static/css/talkwithtito.css";
+import coming_soon from "@/public/static/images/ConversAItionELLE/coming_soon.png";
+import tito_speak from "@/public/static/images/ConversAItionELLE/tito.png";
+import Image from "next/image";
 
-export default function TalkWithTitoGame() {
-    console.log("Tito Here");
-    return(
-        <div className="w-full h-32 flex justify-center bg-blue-200">Hello World I am Tito</div>
+export default function talkwithtito(){
+  const titoStatements:string[] = ['Tito is creating a new dish', 'Tito is freshening up', 'Tito is taking a nap']
+  const titoStatementsRef = useRef<string[]>(titoStatements)
+
+  // Cycle through Tito Statements (practice for loading screen)
+  const [statement, setStatement] = useState<string>(titoStatementsRef.current[0]);
+  const [index, setIndex] = useState<number>(1);
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setStatement(titoStatementsRef.current[index]);
+      setIndex((prev)=> (prev+1) % titoStatementsRef.current.length);
+    }, 4000)
+
+    return () => clearInterval(interval);
+  }, [index])
+
+  return (
+    <div className="header">
+    <div className="body">
+      <div className="game-container">
+        <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background"/>
+        <h2 className="text-overlay">{statement}</h2>
+        <h1 className="in-progress-overlay">Coming Soon</h1>
+        <Image src={tito_speak} alt="TalkWithTito placeholder" className="tito-overlay"/>
+      </div>
+        
+    </div>
+    </div>
     );
+
 }
+
