0f1723921b7e4be2e3646200bdec26b6ec49764c
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2025-02-23 00:39:41 -0500

    Small change

--- templates/pages/games/talkwithtito.tsx
+++ templates/pages/games/talkwithtito.tsx
@@ -59,54 +59,56 @@ export default function TalkWithTito() {
   }, [index])
 
   return (
-    <div className="relative w-full mt-2 mb-2 flex justify-center">
-      <button onClick={() => setIsLoading(!isLoading)} className="absolute top-10 right-0 w-10 h-10 bg-blue-700"></button>
-      {!playClicked ?
-        <div className="relative w-[80%] h-fit border-2 border-black"> {/*Main container for start menu*/}
-          <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background" />
-          {
-            isLoading ?
-              <Image src={tito_speak} alt="TalkWithTito placeholder" className="tito-overlay" />
-              :
-              <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[45%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
-          }
-          <div className="absolute top-[11.5%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
-                        text-white text-2xl md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">{isLoading ? statement : "Talk With Tito"}</div>
-          <div className={`
-                          absolute top-[80%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
-                        text-white md:text-4xl font-bold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover
-                          ${!isLoading ? "hover:bg-[#816031] hover:cursor-pointer" : ""}`}
-            onClick={handlePlayClick}>
-            {isLoading ? "Loading..." : "Play!"}
+    <div className="talkwithtito-body">
+      <div className="relative w-full mt-2 mb-2 flex justify-center">
+        <button onClick={() => setIsLoading(!isLoading)} className="absolute top-10 right-0 w-10 h-10 bg-blue-700"></button>
+        {!playClicked ?
+          <div className="relative w-[80%] h-fit border-2 border-black"> {/*Main container for start menu*/}
+            <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background" />
+            {
+              isLoading ?
+                <Image src={tito_speak} alt="TalkWithTito placeholder" className="tito-overlay" />
+                :
+                <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[45%] left-[50%] -translate-x-1/2 -translate-y-1/2" />
+            }
+            <div className="absolute top-[11.5%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
+                          text-white text-2xl md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">{isLoading ? statement : "Talk With Tito"}</div>
+            <div className={`
+                            absolute top-[80%] left-[50%] w-fit -translate-x-1/2 -translate-y-1/2
+                          text-white md:text-4xl font-bold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover
+                            ${!isLoading ? "hover:bg-[#816031] hover:cursor-pointer" : ""}`}
+              onClick={handlePlayClick}>
+              {isLoading ? "Loading..." : "Play!"}
+            </div>
           </div>
-        </div>
-        :
-        <div className="bg-white relative w-[80%] h-fit border-2 border-black"> {/*Main container for chat menu*/}
-          <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background" />
-            <div className="absolute top-[11.5%] left-[62.5%] w-fit -translate-x-1/2 -translate-y-1/2
-            text-white md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">Welcome, [username]</div>
-            <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[40%] left-[62.5%] -translate-x-1/2 -translate-y-1/2" />
-            <div className="absolute top-[70%] left-[62.5%] w-fit -translate-x-1/2 -translate-y-1/2
-            text-white md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">Pick a module to get started!</div>
-          <div className="absolute top-0 left-0 h-full border-r-2 border-black">
-            <Image src={chatBackground} alt="Chat Background" className="game-background" />
-            <div className="text-white w-full h-full absolute top-0 left-0 flex flex-col justify-between">{/*Main container for text potion of chat menu*/}
-              <div className="w-full py-[0.5em] flex justify-center irish-grover md:text-2xl border-b-2 border-white"> {/*Container for username section*/}
-                [username]
-              </div>
-              <div className="w-full flex justify-center"></div>{/*Container for modules section*/}
-              <div className="w-full flex justify-between items-center irish-grover md:text-2xl border-t-2 border-white">
-                <button className="md:text-2xl ml-2 flex items-center py-3" onClick={handleExitClick}>
-                  <Image src={logoutIcon} alt="Exit" className="mr-2"/>
-                  <div className="hidden md:block">Exit Chat</div>
-                </button>
-                <Image src={settingsIcon} alt="Settings" className="mr-2 h-8 w-8 md:h-10 md:w-10 hover:cursor-pointer" onClick={openSettings}/>
+          :
+          <div className="bg-white relative w-[80%] h-fit border-2 border-black"> {/*Main container for chat menu*/}
+            <Image src={coming_soon} alt="TalkWithTito placeholder" className="game-background" />
+              <div className="absolute top-[11.5%] left-[62.5%] w-fit -translate-x-1/2 -translate-y-1/2
+              text-white md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">Welcome, [username]</div>
+              <Image src={happyTito} alt="Tito is ready" className="absolute w-[35%] top-[40%] left-[62.5%] -translate-x-1/2 -translate-y-1/2" />
+              <div className="absolute top-[70%] left-[62.5%] w-fit -translate-x-1/2 -translate-y-1/2
+              text-white md:text-4xl font-semibold whitespace-nowrap select-none bg-[#997c54] py-2 px-6 border-2 border-black rounded irish-grover">Pick a module to get started!</div>
+            <div className="absolute top-0 left-0 h-full border-r-2 border-black">
+              <Image src={chatBackground} alt="Chat Background" className="game-background" />
+              <div className="text-white w-full h-full absolute top-0 left-0 flex flex-col justify-between">{/*Main container for text potion of chat menu*/}
+                <div className="w-full py-[0.5em] flex justify-center irish-grover md:text-2xl border-b-2 border-white"> {/*Container for username section*/}
+                  [username]
+                </div>
+                <div className="w-full flex justify-center"></div>{/*Container for modules section*/}
+                <div className="w-full flex justify-between items-center irish-grover md:text-2xl border-t-2 border-white">
+                  <button className="md:text-2xl ml-2 flex items-center py-3" onClick={handleExitClick}>
+                    <Image src={logoutIcon} alt="Exit" className="mr-2"/>
+                    <div className="hidden md:block">Exit Chat</div>
+                  </button>
+                  <Image src={settingsIcon} alt="Settings" className="mr-2 h-8 w-8 md:h-10 md:w-10 hover:cursor-pointer" onClick={openSettings}/>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      }
+        }
 
+      </div>
     </div>
   );
 }
