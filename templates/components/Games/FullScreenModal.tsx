cf2e086fb8611b3a9684a6d0c64d042c2fc4171c
awilliams225 <ashwilliams255@gmail.com>
2025-02-24 17:04:52 -0500

    Fixing some z-indexes

--- templates/components/Games/FullScreenModal.tsx
+++ templates/components/Games/FullScreenModal.tsx
@@ -23,7 +23,7 @@ function App() {
                 toggle={toggle}
                 modalTransition={{ timeout: 2000 }}
                 fullscreen={true}
-                zIndex={100}
+                zIndex={1050}
                 >
                 <ModalBody>
                     <p>Simple modal with a modal body...</p>
