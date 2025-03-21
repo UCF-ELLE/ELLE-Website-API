29963e59df52b1923163b42ecf8f090721c5abb4
johnfletcher <markuscabrera.cs1@gmail.com>
2025-03-21 10:34:55 -0400

    DB updates for metadata field

--- resources/conversationElle/docs/convoSchema.sql
+++ resources/conversationElle/docs/convoSchema.sql
@@ -1,10 +1,3 @@
--- New SQL table, using correct convention, python float support
--- userID referencing user table via FK
-
--- TODO: add metadata field for messages?
--- Add new metadata field for each message?
--- add words used inccorectly?
-
 CREATE TABLE `chatbot_sessions` (
     `chatbotId` INT AUTO_INCREMENT PRIMARY KEY,              
     `userId` INT NOT NULL,
@@ -25,6 +18,7 @@ CREATE TABLE `messages` (
     `moduleId` INT NOT NULL,
     `source` ENUM('llm', 'user') NOT NULL,
     `value` TEXT NOT NULL,
+    `metadata` JSON NOT NULL,
     `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`chatbotId`) REFERENCES `chatbotSessions`(`chatbotId`) ON DELETE CASCADE
     FOREIGN KEY (`userId`) REFERENCES `user`(`userId`) ON DELETE CASCADE,
