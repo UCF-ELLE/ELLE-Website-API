d4f4f566a2c5c63111d092338d4f943e7ca750ea
TTrumpet <tran.tinau@gmail.com>
2025-03-20 22:01:52 -0400

    llm reformat and updates

--- resources/conversationElle/config.py
+++ resources/conversationElle/config.py
@@ -6,31 +6,35 @@ curr_path = os.path.abspath(__file__)
 curr_dir = os.path.dirname(curr_path)
 par_dir = str(Path(curr_dir).parent)
 os.chdir(par_dir)
-model_path = os.path.join(par_dir, "Qwen2.5-7B-Instruct")
+#model_path = os.path.join(par_dir, "Qwen2.5-7B-Instruct")
+model_path = "http://10.200.8.216:8000/generate"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # These need to be later pulled from the database.
 language = "Spanish"
+language_list = ["english", "spanish", "portuguese"]
 level = "College"
 name = "FunnyMonkey"
-topic = "Kitchen"
-vocab_list = {"cocinar", "lavar", "moler"}
+#vocab_list = {"cocinar", "lavar", "moler"}
 vocal_percent = 0.5
 
 # Reading list of background files
-background_directory = "./assets/backgrounds"
+background_directory = "resources/conversationElle/assets/backgrounds"
 background_files = os.listdir(background_directory)
 background_files = [f for f in background_files if os.path.isfile(background_directory+'/'+ f) and not '.py' in f]
 
 # Reading list of music files
-music_directory = "./assets/music"
+#music_directory = "./assets/music"
+music_directory = "templates/public/TitoAudios"
 music_files = os.listdir(background_directory)
 music_files = [f for f in music_files if os.path.isfile(music_directory+'/'+ f) and not '.py' in f]
 
 json_string = """{\"thought\": # you should always think about what you need to do, \"tool\": # the 
 name of the tool. This must be one of: [test_function], \"tool_input\": # the input to the tool}"""
 
+'''
+# original prompt
 main_prompt = f"""
     You are a chatty parrot named Tito. Your goal is to teach {language} to people 
     the school level {level}. The person you are talking to is named {name}. You need 
@@ -38,6 +42,19 @@ main_prompt = f"""
     inquiries as best as you can, if you do not know the answer simply state you do not know. 
     You and {name} need to go over these vocabulary words: {vocab_list}. Every response you 
     generate must be in the following JSON format: {json_string}."""
+'''
+# edited prompts
+main_prompt = f"""
+    You are a chatty parrot named Tito. Your goal is to teach {language} to people 
+    the school level {level}. The person you are talking to is named {name}. You need 
+    to only speak in {language}. You should answer inquiries as best as you can, 
+    if you do not know the answer simply state you do not know. 
+    Every response you generate must be in the following JSON format: {json_string}."""
+
+identify_language_prompt = f"""
+    Your goal is to identify the language the user is talking in.
+    Only respond with one of the languages in the following list:
+    {language_list}."""
 
 choose_background = """You need to always respond in the following JSON format:
     #{\"file\": # one file name from: """ + background_files + """}."""
---
+++ resources/conversationElle/llm_helpers.py
@@ -0,0 +1,52 @@
+import string
+import config
+from resources.conversationElle.llm_functions import handleMessage
+
+def test_function(text: str) -> str:
+    translations = "Hola, this function was called"
+    return translations.get(text.lower())
+
+def count_words(text: str, vocab_list: list, vocab_dict=None) -> dict:
+    '''
+    This function counts the words used in a given string and stores
+    the number of times each vocabulary word is used using a dictionary
+    data structure.
+
+    Parameters:
+        text: String corresponding to the text inputted to the LLM
+        vocab_list: List corresponding to the required vocabulary word list
+        vocab_dict: Dictionary of existing word usage from previous sessions
+
+    Returns:
+        Dictionary in the form: {vocab_word: number of times used}
+    '''
+    # Initialize new dict
+    if vocab_dict == None:
+        vocab_dict = {word: 0 for word in vocab_list}
+    
+    # Case that teacher has added new words
+    if not len(vocab_dict.keys()) == len(vocab_list):
+        for word in vocab_list:
+            if word not in vocab_dict:
+                vocab_dict[word] = 0
+    
+    # Parse string to see if vocab word was used
+    clean_text = ''.join([char for char in text if char not in string.punctuation])
+    for word in clean_text.split(" "):
+        if word in vocab_list:
+            vocab_dict[word] += 1
+    
+    return vocab_dict
+
+def grade_grammar(student_input: str):
+    '''
+    This function grades the use of the student's grammar in the chosen language.
+    '''
+    prompt = f"You are being given a sentence from a student. Grade the student's grammar on a scale of 1-10. \
+            The language the student is using is {config.language} Identify and explain any grammatical errors in the text. \
+            Explain your reasoning for the score you give the student. For each error, \
+            provide an example of the correct usage to help the student understand the mistake. Provide your feedback in the \
+            following format: Score: [your score]. Error: [Description of error]. Correction: [Corrected version]. \
+            Explanation: [Why it was wrong and how to fix it]. Here is the student's input: {student_input}"
+    
+    handleMessage(prompt)
