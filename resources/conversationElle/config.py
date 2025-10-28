118b2f5352d8687a2badcd6d004906d30cc43eb7
PrototypeProto <jo338218@ucf.edu>
2025-10-28 02:51:36 -0400

    port change

--- resources/conversationElle/config.py
+++ resources/conversationElle/config.py
@@ -7,7 +7,7 @@ curr_dir = os.path.dirname(curr_path)
 par_dir = str(Path(curr_dir).parent)
 os.chdir(par_dir)
 #model_path = os.path.join(par_dir, "Qwen2.5-7B-Instruct")
-model_path = "http://127.0.0.1:8080/completion"
+model_path = "http://127.0.0.1:8083/completion"
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Using external LLM server, no local device needed
 device = "cpu"
