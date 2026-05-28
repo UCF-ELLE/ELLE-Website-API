d20279011bb8399cd4f4bf12b6356ee67221c218
Zachary Trenary <zmichaeltrenary@gmail.com>
2026-05-27 20:39:30 -0400

    hopefully fixed all of tts issues

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_mail import Mail
-from db import mysql
+# from db import mysql
 from db_utils import *
 from utils import create_response
 from pathlib import Path
