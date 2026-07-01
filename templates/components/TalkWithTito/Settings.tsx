6d5e75fff5aa9bba4aff935f884abea48fe7ecea
Christian <ch322047@ucf.edu>
2026-07-01 11:57:15 -0400

    resolved compilation issue, missing bracket

--- templates/components/TalkWithTito/Settings.tsx
+++ templates/components/TalkWithTito/Settings.tsx
@@ -86,6 +86,7 @@ export default function Settings(props: propsInterface) {
 
   const handleAIChoice = () => {
     onSetAIChoice(!titoMusicChoice);
+  };
 
   useEffect (() => {
     setPlaylist(parentPlaylist)
