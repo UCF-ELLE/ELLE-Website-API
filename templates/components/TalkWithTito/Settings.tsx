2c288ff7f64e804af55042815afd92347f2f21fe
Christian <ch322047@ucf.edu>
2026-07-01 12:10:16 -0400

    resolved compilation issue, missing bracket

--- templates/components/TalkWithTito/Settings.tsx
+++ templates/components/TalkWithTito/Settings.tsx
@@ -87,7 +87,7 @@ export default function Settings(props: propsInterface) {
     onSetAIChoice(!titoMusicChoice);
   };
 
-  }
+  
 
   // const handleTtsMute = () => {
   //   onSetTtsMuted(!ttsMuted);
